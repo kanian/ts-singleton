b94e712d754a31103fdc08bd8739a27434cfa9ea
Patrick Assoa Adou <kanian77@gmail.com>
2020-02-10 23:03:51 +0000

    feat: :star: Add index.ts

--- src/singleton.ts
+++ src/singleton.ts
@@ -1,13 +1,13 @@
 export function singleton(target: any) {
   // save a reference to the original constructor
-  let original = target
-  let instance = undefined
+  let original = target;
+  let instance = undefined;
   // wrap orginal constructor with singleton behaviour
   var f: any = function(...args) {
-    instance = instance === undefined ? new original(...args) : instance
-    return instance
-  }
+    instance = instance === undefined ? new original(...args) : instance;
+    return instance;
+  };
   // set f's prototype to orginal's prototype so f keeps original's type
-  f.prototype = original.prototype
-  return f
+  f.prototype = original.prototype;
+  return f;
 }
